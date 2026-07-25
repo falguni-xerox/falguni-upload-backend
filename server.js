781595c3c8d4604d5b18473a9e794c24a3b80791
falguni-xerox <sunilthakor3693@gmail.com>
2026-07-25 13:52:36 +0530

    fix: change port to 10000 for render

--- server.js
+++ server.js
@@ -249,7 +249,7 @@ app.use((err, req, res, next) => {
 // ======================================
 
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 10000;
 
 
 app.listen(PORT, "0.0.0.0", () => {
